c691a8db8039ad739949c08b1753f3a259080b90
烏 <58228327+6lyxt@users.noreply.github.com>
2022-09-27 21:34:24 +0200

    fixing lil earrape issue

--- js/app.js
+++ js/app.js
@@ -1,27 +1,20 @@
 var playing = false;
 function playMusic() {
-    var audio = new Audio('media/music.mp3');
     var video = document.getElementById('video');
+    video.volume = 0.05;
     if (playing == false) {
         document.getElementsByClassName('muted')[0].innerText = '';
         video.play();
-        audio.play();
-        audio.volume = 0.05;
+        video.volume = 0.007;
         playing = true;
         video.style = "filter: brightness(100%) blur(0px)";
     } else {
         video.pause();
-        audio.pause();
     }
     video.addEventListener("ended", function() {
         video.currentTime = 0;
         video.play();
     })
-
-    audio.addEventListener("ended", function(){
-        audio.currentTime = 0;
-        audio.play();
-   });
 }
 
 var intervalID = window.setInterval(randomText, 10000);
