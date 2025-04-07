f774a19e8f02121c94e56a187f8931fa3322fc2d
jd <jd@cic.at>
2025-04-07 08:48:06 +0200

    feat: fetch and display latest GitHub projects in WorkComponent

--- src/components/work.tsx
+++ src/components/work.tsx
@@ -1,22 +1,35 @@
+import React, {useEffect, useState} from "react";
+import {Col, Row} from "react-bootstrap";
+
 const WorkComponent = () => {
+
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        fetch("https://api.github.com/users/6lyxt/repos?sort=updated&direction=desc&per_page=4").then(res => {
+            return res.json();
+        }).then(data => {
+            setProjects(data);
+        })
+    }, [])
+
     return (
         <>
-            <h1>recent work</h1>
+            <h1>recent work (latest github projects)</h1>
             <ul>
-                <li>
-                    <b>reassign</b>
-                    <p><u>reassign.team</u> is a web-based project managment tool, which should help project managers assigning tasks to the fitting person based on <i>personality traits</i>, <i>working habits</i>, <i>weekly cost</i> and <i>weekly working hours.</i></p>
-                </li>
-                <li>
-                    <b>secretpages</b>
-                    <p><u>secretpages</u> is a proof-of-concept pastebin clone, which is based on markdown driven files.</p>
-                </li>
-                <li>
-                    <b>httpbro</b>
-                    <p><u>httpbro</u> is a http library written in typescript</p>
-                </li>
+                {projects.map((project: any) => {
+                    return (
+                        <li key={project.id} className={'mb-3'}>
+                            <a href={project.html_url}><b>{project.name}</b></a><br/>
+                            <span>{project.description}</span><br/>
+                            <span className={'border-bottom'}>{project.language}</span>
+                        </li>
+                    )
+                })}
             </ul>
-            <p>you can checkout all my projects <u><a href={'https://github.com/6lyxt'} target={'_blank'}>here</a></u></p>
+            <p>you can checkout all my projects <u><a href={'https://github.com/6lyxt'}
+                                                      target={'_blank'}>here</a></u>
+            </p>
         </>
     )
 }
